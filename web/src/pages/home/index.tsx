c0384ce06d6c3a4690b5f3fbff6cf105bb4bf959
Eduardo Siridakis <siri@DESKTOP-EEJ8BGT.localdomain>
2022-11-21 20:20:04 -0300

    teste de consumo de api graphql

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,22 +1,46 @@
-import { useQuery } from "@apollo/client";
-import { Box, Heading } from "rebass";
+import { useQuery, useLazyQuery } from "@apollo/client";
+import { Box, Button, Heading } from "rebass";
 import { LIST_USERS } from "../../graphql/queries/list-users";
+import { GET_WEBSITES } from "../../graphql/queries/get-websites";
 import { User } from "../../typings/user";
 import { UserCard } from "./components";
+import React from "react";
 
 export const HomePage = () => {
   const { data, loading, error } = useQuery<{ users: User[] }>(LIST_USERS);
+  const [randomQuery, {data: dados, loading: carregando, error: erro}] = useLazyQuery<{users: User[] }>(GET_WEBSITES);
+
 
   if (loading) return <>Carregando...</>;
   if (error || !data?.users) return <>Deu ruim :(</>;
 
   const { users } = data;
 
+  async function handleButtonClick(e: React.FormEvent) {
+    e.preventDefault()
+    const dados = await randomQuery()
+
+    if (carregando) return <>Carregando...</>;
+    if (erro || !dados?.data) return <>Deu ruim :(</>;
+
+    const websites = dados?.data?.users?.map((element) => element?.website)
+    const pickANumber = Math.floor(Math.random() * 10)
+    const siteSorteado = websites[pickANumber]
+
+    window.open("https://www."+siteSorteado, '_blank')
+
+  }
+
+  
   return (
     <Box p={60}>
       <Heading fontSize={72} mb={54}>
         Todos usuários
       </Heading>
+
+      <Button mb={4} onClick={(e) => handleButtonClick(e)}>
+        Randomize website
+      </Button>
 
       {users.map(({ id, name, username, website }) => (
         <UserCard
